cfc199b7bf5cdc6bf32b54ca87f268a1777c3db5
AFRINA AMIR NIRJONA <102577685+AFRINA5@users.noreply.github.com>
2023-03-14 23:18:47 +0600

    Add files via upload
    Midterm Lab Task
    18-38748-3

---
+++ MID/Task3/profilePicture.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  
+</head>
+<body>
+
+<form action="/action_page.php" align="center">
+<br><br><br>
+<img src="dp.png" alt="DP" width="150" height="150"> <br>
+  <input type="file" id="myFile" name="filename">\<br>
+  <br>
+  <input type="submit">
+</form>
+
+
+</body>
+</html>
---
+++ MID/Task4/homepage.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Home</title>
+</head> 
+<body>
+	<?php include 'Top.php';?>
+	<fieldset>
+		<h1 class = "color_C">Welcome!!</h1>
+	</fieldset>
+	
+</body>
+</html>
